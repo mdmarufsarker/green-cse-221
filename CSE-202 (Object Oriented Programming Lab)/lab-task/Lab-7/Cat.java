26c7d9f819a3274b42b60aa64f7a5b6e0a9d5433
Md Maruf Sarker <98809108+maruf-sarker@users.noreply.github.com>
2023-05-09 13:05:07 +0600

    Add files via upload

---
+++ CSE-202 (Object Oriented Programming Lab)/lab-task/Lab-7/Cat.java
@@ -0,0 +1,10 @@
+public class Cat implements Introduction{
+
+    public void sound(){
+        System.out.println("Meow");
+    }
+
+    public void name(){
+        System.out.println("My name is :D");
+    }
+}
---
+++ CSE-202 (Object Oriented Programming Lab)/lab-task/Lab-7/Dog.java
@@ -0,0 +1,6 @@
+public class Dog extends Name implements Animal{
+
+    public void sound(){
+        System.out.println("Dog");
+    }
+}
