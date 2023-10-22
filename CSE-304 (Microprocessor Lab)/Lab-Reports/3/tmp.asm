1d41b857bdfe2062f4a3af23b525722c9ae5b9cb
mdmarufsarker <mdmarufsarker2021@gmail.com>
2023-10-23 02:13:11 +0600

    micro lab-4 report done

--- CSE-304 (Microprocessor Lab)/Lab-Reports/3/tmp.asm
+++ CSE-304 (Microprocessor Lab)/Lab-Reports/3/tmp.asm
@@ -29,49 +29,30 @@ main proc
 
     ; perform the operation
     ; brute force approach
-    
-    ; store first value of id string into a register
-    mov al, id
-    mov si, 2
-    mov my_name[si], al
-
-    ; store second value of id string into a register
-    mov bl, id + 1
     mov si, 2
     mov my_name[si], '2'
 
-    ; store third value of id string into a register
-    mov al, id + 2
+    mov si, 2
+    mov my_name[si], '2'
+
     mov si, 1
     mov my_name[si], '1'
 
-    ; store fourth value of id string into a register
-    mov bl, id + 3
     mov si, 0
     mov my_name[si], '0'
 
-    ; store fifth value of id string into a register
-    mov al, id + 4
     mov si, 0
     mov my_name[si], '0'
 
-    ; store sixth value of id string into a register
-    mov bl, id + 5
     mov si, 2
     mov my_name[si], '2'
-
-    ; store seventh value of id string into a register
-    mov al, id + 6
+    
     mov si, 0
     mov my_name[si], '0'
 
-    ; store eighth value of id string into a register
-    mov bl, id + 7
     mov si, 6
     mov my_name[si], '6'
 
-    ; store ninth value of id string into a register
-    mov al, id + 8
     mov si, 3
     mov my_name[si], '3'
 
