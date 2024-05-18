5f884bbb4939c308bd332d9ec60715dc7874e50d
mdmarufsarker <mdmarufsarker2021@gmail.com>
2024-05-19 02:28:43 +0600

    dc project half done

---
+++ CSE-308 (Data Communication Lab)/Project/realtime-chat-app/src/components/List/Chatlist/Chatlist.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./Chatlist.css";
+
+const Chatlist = () => {
+  const [addMode, setAddMode] = useState(false);
+  return (
+    <div className="chatList">
+      <div className="search">
+        <div className="searchBar">
+          <img src="./search.png" alt="" />
+          <input type="text" placeholder="search" />
+        </div>
+        <img
+          src={addMode ? "./minus.png" : "./plus.png"}
+          alt=""
+          className="add"
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Abu Sayed</span>
+            <p>Project Ready na re vai</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Abu Sayed</span>
+            <p>Project Ready na re vai</p>
+        </div>
+      </div>
+      <div className="item">
+        <img src="./avatar.png" alt="" />
+        <div className="texts">
+            <span>Abu Sayed</span>
+            <p>Project Ready na re vai</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chatlist;
